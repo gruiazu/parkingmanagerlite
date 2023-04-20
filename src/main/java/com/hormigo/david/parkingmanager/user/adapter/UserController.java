d6f239e0c0c152795ef802a419a89c863990a9fa
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-04-20 09:53:18 +0200

    Formulario Usuarios

--- src/main/java/com/hormigo/david/parkingmanager/user/adapter/UserController.java
+++ src/main/java/com/hormigo/david/parkingmanager/user/adapter/UserController.java
@@ -3,7 +3,10 @@ package com.hormigo.david.parkingmanager.user.adapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.hormigo.david.parkingmanager.user.domain.*;
 import com.hormigo.david.parkingmanager.user.service.UserService;
 
 @Controller
@@ -20,5 +23,17 @@ public class UserController {
         model.addAttribute("users", this.userService.getAll());
         return "user/list";
     }
-    
+
+    @GetMapping("/newUser")
+    public String showUserCreateForm(Model model) {
+        UserDao userDao = new UserDao();
+        model.addAttribute("userDao", userDao);
+        return "user/createform";
+    }    
+
+    @PostMapping("/newUser")
+    public String showUserCreateForm(@ModelAttribute("userDao") UserDao userDao) {
+        this.userService.register(userDao);
+        return "redirect:/users";
+    }   
 }
