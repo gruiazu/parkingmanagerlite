1a634fe893be030435decdf7ad4f07398c4fc391
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-04-22 18:29:20 +0200

    Reordenación imports

--- src/test/java/com/hormigo/david/parkingmanager/user/service/UserServiceImplTest.java
+++ src/test/java/com/hormigo/david/parkingmanager/user/service/UserServiceImplTest.java
@@ -1,36 +1,45 @@
 package com.hormigo.david.parkingmanager.user.service;
 
 
-import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
-import java.util.List;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 import com.hormigo.david.parkingmanager.core.exceptions.UserExistsException;
-import com.hormigo.david.parkingmanager.user.domain.*;
+import com.hormigo.david.parkingmanager.user.domain.Role;
+import com.hormigo.david.parkingmanager.user.domain.User;
+import com.hormigo.david.parkingmanager.user.domain.UserDao;
+import com.hormigo.david.parkingmanager.user.domain.UserRepository;
 
 public class UserServiceImplTest {
     @Test
     void testGetAll() {
-        List<User> expected = new ArrayList<>();
+        final List<User> expected = new ArrayList<>();
         expected.add(new User("david@correo","David","Hormigo","Ramirez",Role.PROFESSOR));
-        UserRepository mockedRepository = mock(UserRepository.class);
+        final UserRepository mockedRepository = mock(UserRepository.class);
         when(mockedRepository.findAll()).thenReturn(expected);
-        UserService service = new UserServiceImpl(mockedRepository);
-        List<User> actual = (List<User>) service.getAll();
+        final UserService service = new UserServiceImpl(mockedRepository);
+        final List<User> actual = (List<User>) service.getAll();
         assertEquals(expected,actual);
     }
 
     @Test
     void testUserAlreadyExists() {
-        User user = new User("david@correo","David","Hormigo","Ramírez",Role.PROFESSOR);
-        UserRepository mockedRepository = mock(UserRepository.class);
+        final User user = new User("david@correo","David","Hormigo","Ramírez",Role.PROFESSOR);
+        final UserRepository mockedRepository = mock(UserRepository.class);
         when(mockedRepository.findByEmail("david@correo")).thenReturn(user);
-        UserService service = new UserServiceImpl(mockedRepository);
+        final UserService service = new UserServiceImpl(mockedRepository);
 
-        UserDao userDao = new UserDao("david@correo", "David","Hormigo", "Ramírez", Role.PROFESSOR);
+        final UserDao userDao = new UserDao("david@correo", "David","Hormigo", "Ramírez", Role.PROFESSOR);
     
         assertThrows(UserExistsException.class,()->{service.register(userDao);});
 
@@ -39,15 +48,15 @@ public class UserServiceImplTest {
     @Test
     void testUserDoesNotExists() {
         //User user = new User("david@correo","David","Hormigo","Ramírez",Role.PROFESSOR);
-        UserRepository mockedRepository = mock(UserRepository.class);
+        final UserRepository mockedRepository = mock(UserRepository.class);
         when(mockedRepository.findByEmail("david@correo")).thenReturn(null);
-        UserService service = new UserServiceImpl(mockedRepository);
+        final UserService service = new UserServiceImpl(mockedRepository);
 
-        UserDao userDao = new UserDao("david@correo", "David","Hormigo", "Ramírez", Role.PROFESSOR);
+        final UserDao userDao = new UserDao("david@correo", "David","Hormigo", "Ramírez", Role.PROFESSOR);
         try {
         service.register(userDao);
         }
-        catch (UserExistsException e) {
+        catch (final UserExistsException e) {
             fail("Ha lanzaado la excepción");
         }
         verify(mockedRepository).save(any(User.class));
